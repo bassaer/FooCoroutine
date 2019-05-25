011bcb00f405a65c2d6f7d4ae5068130389facb8
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-05-25 19:46:03 +0900

    hoge

--- app/src/main/java/com/github/bassaer/foocoroutine/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/foocoroutine/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.github.bassaer.foocoroutine
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        Log.d(javaClass.simpleName, "before GlobalScope")
+        GlobalScope.launch {
+            Log.d(javaClass.simpleName, "running GlobalScope")
+            textView.text = "Foo!!!!!!!!!"
+        }
+        Log.d(javaClass.simpleName, "after GlobalScope")
     }
+
 }

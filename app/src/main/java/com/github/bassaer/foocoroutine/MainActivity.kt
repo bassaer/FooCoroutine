09c1ce2a4fa88ac2da6b95253f9787278950d0ef
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-05-25 20:39:55 +0900

    update

--- app/src/main/java/com/github/bassaer/foocoroutine/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/foocoroutine/MainActivity.kt
@@ -3,7 +3,9 @@ package com.github.bassaer.foocoroutine
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -14,10 +16,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.text_view)
+        println("thread1 name = ${Thread.currentThread().name}") // thread1 name = main
         Log.d(javaClass.simpleName, "before GlobalScope")
-        GlobalScope.launch {
+        GlobalScope.launch(Dispatchers.Unconfined) {
             Log.d(javaClass.simpleName, "running GlobalScope")
             textView.text = "Foo!!!!!!!!!"
+            println("thread2 name = ${Thread.currentThread().name}") // thread2 name = main
+            Toast.makeText(applicationContext, "foo!!!!!!!", Toast.LENGTH_SHORT).show()
+        }
+        /*
+        GlobalScope.launch(Dispatchers.Main) {
+            // will crash
+        }
+        */
+        GlobalScope.launch {
+            println("thread3 name = ${Thread.currentThread().name}") // thread3 name = DefaultDispatcher-worker-1
         }
         Log.d(javaClass.simpleName, "after GlobalScope")
     }
